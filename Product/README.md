5d48db2ef5f5b4bf6100861b51fa451501fe080d
Kamran Aqeel <kab935@uregina.ca>
2024-11-22 23:02:30 -0600

    Ense374Project

--- Product/README.md
+++ Product/README.md
@@ -16,3 +16,38 @@ A web hosted on MongoDB using JavaScript, HTML, CSS, PHP, EJS and Mongo.
 - ✔️ 
 - ✔️ 
 - ⏳ Assignment 4
+
+
+// title
+let title = document.getElementById("post-title");
+title.addEventListener("blur", postTitleHandler);
+
+// content
+let content = document.getElementById("post-text");
+content.addEventListener("blur", postContentHandler);
+
+// submit button
+let form = document.getElementById("create-form");
+form.addEventListener("submit", validatePost);
+
+// dynamic counter
+let blogPostTitle = document.getElementById("post-title");
+blogPostTitle.addEventListener("input", (event) => charCounter(event, 100));
+
+// dynamic counter for body text
+let blogPostTextarea = document.getElementById("post-text");
+blogPostTextarea.addEventListener("input", (event) => charCounter(event, 2000));
+
+
+const searchInput = document.querySelector("[data-search]")
+
+let trails = []
+
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  trails.forEach(trail => {
+    const isVisible =
+      trail.location.toLowerCase().includes(value)
+    trail.element.classList.toggle("hide", !isVisible)
+  })
+})
--- Product/public/js/eventRegisteSearchTrail.js
+++ Product/public/js/eventRegisteSearchTrail.js
@@ -1,49 +1,24 @@
-const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
+const trailCardTemplate = document.querySelector("[data-trail-template]")
+const trailCardContainer = document.querySelector("[data-trail-cards-container]")
 const searchInput = document.querySelector("[data-search]")
-
-let users = []
-
+let trails = []
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
-  users.forEach(user => {
+  trails.forEach(trail => {
     const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
-    user.element.classList.toggle("hide", !isVisible)
+    trail.location.toLowerCase().includes(value)
+    trail.element.classList.toggle("hide", !isVisible)
   })
 })
 
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://mocki.io/v1/a5688029-adbc-4203-8275-1fc94132f6b9")
   .then(res => res.json())
   .then(data => {
-    users = data.map(user => {
-      const card = userCardTemplate.content.cloneNode(true).children[0]
+    trails = data.map(trail => {
+      const card = trailCardTemplate.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
-      header.textContent = user.name
-      body.textContent = user.email
-      userCardContainer.append(card)
-      return { name: user.name, email: user.email, element: card }
+      header.textContent = trail.location
+      trailCardContainer.append(card)
+      return { location: trail.location, element: card }
     })
 })
-
-// title
-let title = document.getElementById("post-title");
-title.addEventListener("blur", postTitleHandler);
-
-// content
-let content = document.getElementById("post-text");
-content.addEventListener("blur", postContentHandler);
-
-// submit button
-let form = document.getElementById("create-form");
-form.addEventListener("submit", validatePost);
-
-// dynamic counter
-let blogPostTitle = document.getElementById("post-title");
-blogPostTitle.addEventListener("input", (event) => charCounter(event, 100));
-
-// dynamic counter for body text
-let blogPostTextarea = document.getElementById("post-text");
-blogPostTextarea.addEventListener("input", (event) => charCounter(event, 2000));
